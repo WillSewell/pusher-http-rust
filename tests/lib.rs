0e98e6a1490e64d250e91913176f512dd13a4c36
Kayle Gishen <k@bkdsw.com>
2020-05-22 23:30:41 -0400

    Reverted `rustfmt`

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,9 +1,8 @@
 extern crate hyper;
-extern crate log;
 extern crate pusher;
+extern crate log;
 extern crate tokio;
-#[cfg(test)]
-#[macro_use]
+#[cfg(test)] #[macro_use]
 extern crate yup_hyper_mock;
 
 use hyper::Client;
@@ -47,74 +46,56 @@ mock_connector!(ChannelUsersRequest {
 #[tokio::test]
 async fn test_error_response_handler() {
     let client = Client::builder().build(BadRequest::default());
-    let pusher = PusherBuilder::new_with_client(client, "1", "2", "3")
-        .host("127.0.0.1")
-        .finalize();
-    let query_params = vec![(
-        "info".to_string(),
-        "user_count,subscription_count".to_string(),
-    )];
-    let res = pusher
-        .channel_with_options("this_is_not_a_presence_channel", query_params)
-        .await;
+    let pusher = PusherBuilder::new_with_client(client, "1", "2", "3").host("127.0.0.1").finalize();
+    let query_params = vec![("info".to_string(), "user_count,subscription_count".to_string())];
+    let res = pusher.channel_with_options("this_is_not_a_presence_channel", query_params).await;
     assert_eq!(res.unwrap_err(), "Error: 400 Bad Request. Cannot retrieve the user count unless the channel is a presence channel")
 }
 
 #[tokio::test]
-async fn test_eb_trigger() {
-    let client = Client::builder().build(TriggerEBTest::default());
-    let pusher = PusherBuilder::new_with_client(client, "1", "2", "3")
-        .host("127.0.0.1")
-        .finalize();
-    let res = pusher.trigger("woot", "yolo", "huh").await;
-    let events = res.unwrap();
-    println!("{:?}", events);
-    let event_id = events.event_ids.unwrap();
+async fn test_eb_trigger(){
+  let client = Client::builder().build(TriggerEBTest::default());
+  let pusher = PusherBuilder::new_with_client(client, "1", "2", "3").host("127.0.0.1").finalize();
+  let res = pusher.trigger("woot", "yolo", "huh").await;
+  let events = res.unwrap();
+  println!("{:?}", events);
+  let event_id = events.event_ids.unwrap();
 
-    assert_eq!(event_id.get("test_channel").unwrap(), "eudhq1809scss2")
+  assert_eq!(event_id.get("test_channel").unwrap(), "eudhq1809scss2")
 }
 
 #[tokio::test]
-async fn test_get_channels() {
-    let client = Client::builder().build(ChannelsRequest::default());
-    let pusher = PusherBuilder::new_with_client(client, "1", "2", "3")
-        .host("127.0.0.1")
-        .finalize();
-    let res = pusher.channels().await;
-    let channels = res.unwrap();
-    let item = channels
-        .channels
-        .get("presence-session-d41a439c438a100756f5-4bf35003e819bb138249-5cbTiUiPNGI")
-        .unwrap();
-    assert_eq!(item.user_count.unwrap(), 1)
+async fn test_get_channels(){
+  let client = Client::builder().build(ChannelsRequest::default());
+  let pusher = PusherBuilder::new_with_client(client, "1", "2", "3").host("127.0.0.1").finalize();
+  let res = pusher.channels().await;
+  let channels = res.unwrap();
+  let item = channels.channels.get("presence-session-d41a439c438a100756f5-4bf35003e819bb138249-5cbTiUiPNGI").unwrap();
+  assert_eq!(item.user_count.unwrap(), 1)
 }
 
 #[tokio::test]
-async fn test_get_channel() {
-    let client = Client::builder().build(ChannelRequest::default());
-    let pusher = PusherBuilder::new_with_client(client, "1", "2", "3")
-        .host("127.0.0.1")
-        .finalize();
-    let res = pusher.channel("presence-for-all").await;
-    let channel = res.unwrap();
-    let user_count = channel.user_count.unwrap();
-    let occupied = channel.occupied.unwrap();
-    let subscription_count = channel.subscription_count.unwrap();
-    assert_eq!(user_count, 1);
-    assert_eq!(occupied, true);
-    assert_eq!(subscription_count, 1)
+async fn test_get_channel(){
+  let client = Client::builder().build(ChannelRequest::default());
+  let pusher = PusherBuilder::new_with_client(client, "1", "2", "3").host("127.0.0.1").finalize();
+  let res = pusher.channel("presence-for-all").await;
+  let channel = res.unwrap();
+  let user_count = channel.user_count.unwrap();
+  let occupied = channel.occupied.unwrap();
+  let subscription_count = channel.subscription_count.unwrap();
+  assert_eq!(user_count, 1);
+  assert_eq!(occupied, true);
+  assert_eq!(subscription_count, 1)
 }
 
 #[tokio::test]
-async fn test_get_channel_users() {
-    let client = Client::builder().build(ChannelUsersRequest::default());
-    let pusher = PusherBuilder::new_with_client(client, "1", "2", "3")
-        .host("127.0.0.1")
-        .finalize();
-    let res = pusher.channel_users("presence-yolo").await;
-    let users = res.unwrap().users;
-    let user_one = &users[0];
-    let user_two = &users[1];
-    assert_eq!(user_one.id, "red");
-    assert_eq!(user_two.id, "blue")
+async fn test_get_channel_users(){
+   let client = Client::builder().build(ChannelUsersRequest::default());
+   let pusher = PusherBuilder::new_with_client(client, "1", "2", "3").host("127.0.0.1").finalize();
+   let res = pusher.channel_users("presence-yolo").await;
+   let users = res.unwrap().users;
+   let user_one = &users[0];
+   let user_two = &users[1];
+   assert_eq!(user_one.id, "red");
+   assert_eq!(user_two.id, "blue")
 }
